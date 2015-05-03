bbb51fe87cc6d4b1ce9846e0788d621444bdd9fb
MelanieJae <mtaurus575@gmail.com>
2015-05-03 16:56:00 +0000

    added .map approach to transform method

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -1,13 +1,10 @@
 class Image
 	
-	def initialize(row1,*rows) 
-		@image = Array[row1,row2,row3,row4]
- 			@row1 = row2
- 			@rows.each do |row|
- 				@row = row
-			end
-		@image = 
-	#use @image = rows with *rows and that's all you need.
+	def initialize(*rows) 
+		@image = []
+ 		rows.each do |row|
+			@image << row
+		end
 	end
 	
 #loop only for blur #1	
@@ -18,13 +15,36 @@ class Image
 # 		end
 # 	end	
 	
- 		def blur
-    		@image.each do |row|   		
-     			row[row.index(1) + 1] == 1
-    			row[row.index(1) - 1] == 1 				
-    		end		
-    	end		
+ 	def transform
+#possible solution #1
+		transform_array = @image.map do |row|
+			row[row.index(1) + 1] == 1
+	    	row[row.index(1) - 1] == 1
+#figure out for top and bottom rows
+	    end
+	end
 
-image = Image.new([0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0])
-image.blur
+# #possible solution #2
+# #first row
+# 	@image.each do |row|
+# 		if @image.index(row) = 0 
+# 			row[row.index(1) + 1] == 1
+# 	    	row[row.index(1) - 1] == 1
+# 			below = row.index(1)
+# 			row[below] == 1	# <=fix this
+# #middle rows
+#     	elsif
+#     	if @image.index(row)    		
+#      		row[row.index(1) + 1] == 1
+#     		row[row.index(1) - 1] == 1
+#     		above_below = row.index(1)
+#     		row[above_below] == 1 		
+#     	end		
+# #last row
+# 		else
+# 			row[row.index(1) + 1] == 1
+#     		row[row.index(1) - 1] == 1   			
+# 	end
 end
+image = Image.new([0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0])
+image.transform
