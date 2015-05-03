11aab35413c5c00d57cd4dcfc60baa8a8ef32b72
MelanieJae <mtaurus575@gmail.com>
2015-05-03 05:35:17 +0000

    added varag to initialize loop

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -1,21 +1,30 @@
 class Image
-	attr_accessor :row1, :row2, :row3, :row4
 	
-	def initialize(row1,row2,row3,row4)
+	def initialize(row1,*rows) 
 		@image = Array[row1,row2,row3,row4]
-			self.row1 = row1
-			self.row2 = row2
-			self.row3 = row3
-			self.row4 = row4
+ 			@row1 = row2
+ 			@rows.each do |row|
+ 				@row = row
+			end
+		@image = 
+	#use @image = rows with *rows and that's all you need.
 	end
 	
-	def output_image		
-		@image.each do |i|
-			row = i.join
-			puts "#{row}"
-		end
-	end	
+#loop only for blur #1	
+# #	def output_image		
+# 		@image.each do |i|
+# 			row = i.join
+# 			puts "#{row}"
+# 		end
+# 	end	
 	
-end
+ 	def blur
+    		@image.each do |row|   		
+     			row[row.index(1) + 1] == 1
+    			row[row.index(1) - 1] == 1 				
+    		end		
+    	end		
+
 image = Image.new([0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0])
-image.output_image
+image.blur
+end
