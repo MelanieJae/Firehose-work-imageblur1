7a95698fb6dfc2ff16dfd6dead4d64c698df28b4
MelanieJae <mtaurus575@gmail.com>
2015-05-10 20:32:28 +0000

    final copy of code with comments

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -1,80 +1,73 @@
 class Image
 
-   	def initialize(row1,*rows)
-      @image = []
-      @row1 = row1
-      @image << @row1
-      @rows = rows
-      @image << @rows
-      
-#    puts @image
-   	end
-
-
-#loop only for blur #1	
-# #	def output_image		
-# 		@image.each do |i|
-# 			row = i.join
-# 			puts "#{row}"
-# 		end
-# 	end	
-	
-	def scan
-#    @row1.each_index {|x| puts x}
-#    @rows.each {|x| puts x}
-
-    @row1.each do |x|
-      @idx = []
-      if x == 1
-        puts @row1.index(x)
-        @idx << @row1.index(x)
-      else
-        @idx << -1
-      end 
+    def initialize(rows)
+      @pixels = rows
     end
+	
+	 def transform
+# make copy of original to modify as I scan through the original
+# without changing it
+      pixels_copy = @pixels.clone
+# loop through each element of the original to scan for 1s; doing reverse 
+# order of rows b/c for some reason:
 
-#    @rows.each do |x|
-      @array = []
-      @array << @rows[0].to_a
-      puts @array
-      puts @array.include?(1)
-#      puts @array.index(1)
-#      end
-#      @array.each do |y|
-#        if y == 1
-#          puts @array.index(y)
-#        end   
-#        puts @rows.index(1)
-#       @temprow = []
-#       @temprow << x
-#       @temprow.each do |y|
-#         if y == 1
-#           puts @temprow.index(y)
-# #         @idx << @rows.index(x)
-#         else
-#           puts -1
-# #         @idx << -1
-#         end
-# #      puts @idx
-#      end
- 	  
-  end
- 	# def transform
-	 # 	new_image = @image.dup 
-  # 		puts new_image
-  # 		x = 1
- 	# 	new_image[x+1] = 1
- 	#     new_image[x-1] = 1
- 	#     # new_image[x][y+1] = 1
- 	#     # new_image[x][y-1] = 1
- 	#     puts new_image
-  #   end
-#	puts @idx	
-#	puts new_image
-#	@image = new_image
-#	end
-
+#   1. pixels_copy is either still being read somehow as opposed to the original (or at the same time as @pixels) or 
+#   2. @pixels is somehow being modified despite the code clearly saying to modify pixels_copy or
+#   3. none of the above but sequence somehow still matters when doing modifications to pixels_copy) so only modifying pixels
+#   that were already passed by in the scan of the original seems to help the most (but not completely)
+ 
+      @pixels.each_index.reverse_each do |x|
+        @pixels[x].each_index do |y|
+          if @pixels[x][y] == 1
+# 1st edge case exception for 1s in the corners; if there is a 1 in the corner
+# it only modifies adjacent pixels on the edge of the array to 1s
+            if x == 0
+              if y == 0            
+                pixels_copy[x+1][y] = 1
+               elsif y == 3
+                pixels_copy[x][y-1] = 1            
+                pixels_copy[x+1][y] = 1
+              end
+            elsif x == 3
+                if y == 0
+                  pixels_copy[x-1][y] = 1
+                elsif y == 3            
+                  pixels_copy[x-1][y] = 1
+                end
+# next exception is edge cases for inner rows; if there is a 1 on the left
+# or right edge of the array but in a middle row, only the three adjacent pixels
+# are changed to 1s                 
+            elsif y == 0
+                pixels_copy[x+1][y] = 1
+                pixels_copy[x-1][y] = 1
+                pixels_copy[x][y+1] = 1
+            elsif y == 3 
+                pixels_copy[x+1][y] = 1
+                pixels_copy[x][y-1] = 1             
+#if no other exceptions are met (i.e. a "1" pixel is a middle x and/or y value, all four adjacent pixels are changed to 1s
+# exception for y = 2 is due to the reverse order of row reading to prevent changing of[x-1][2] pixels to 1s that aren't already
+# because the loop hasn't reached that adjacent pixel yet and may incorrectly change surrounding pixels to 1s. 
+            elsif y == 2
+              pixels_copy[x][y+1] = 1
+              pixels_copy[x][y-1] = 1            
+              pixels_copy[x+1][y] = 1
+         
+            else
+              pixels_copy[x][y+1] = 1
+              pixels_copy[x][y-1] = 1            
+              pixels_copy[x+1][y] = 1
+              pixels_copy[x-1][y] = 1  
+            end
+            
+          end
+        end
+      
+      end    
+    @pixels = pixels_copy
+    puts @pixels
+    end
+  
 end
-image = Image.new([0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0])
-image.scan
-#image.transform
+
+image = Image.new([[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]])
+image.transform
