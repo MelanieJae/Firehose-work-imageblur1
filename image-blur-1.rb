657345e23f9a86b83845d43045a9a80e3bf1f54f
MelanieJae <mtaurus575@gmail.com>
2015-05-21 22:13:48 +0000

    imageblur 2 w/o reverse read and with reference to array length in conditions

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -15,27 +15,27 @@ class Image
       @pixels.each_index do |x|
           @pixels[x].each_index do |y|     
               if @pixels[x][y] == 1
-                if y-1 >= 0 and y != 3
+                if y-1 >= 0 and y != (@pixels[x].length - 1)
                   pixels_copy[x][y-1] = 1
 # allows changing of pixels on the inside without interfering with already-changed 1s
 # or 0s that shouldn't be changed.
 # e.g. when the first 1 is at [x][y] and the 
 # second 1 is at [x+1][y+2] or possibly vice versa, second 1 is at [x-1][y+2], connection
 # to manhattan distance suggestion re: absolute value of differences in x and y to trace
-# the boundary of the blur?
-                  if x-1 >= 0 and x != 3
+# the boundary of the blur? (also connection to knight's move in chess game?)
+                  if x-1 >= 0 and x != (@pixels.length - 1)
                     pixels_copy[x+1][y+1] = 1
                   end
                 end
-                if x-1 >= 0 and x != 3
+                if x-1 >= 0 and x != (@pixels.length - 1)
                   pixels_copy[x-1][y] = 1
                 end        
-                if y == 3 and x!= 3
+                if y == (@pixels[x].length - 1) and x!= (@pixels.length - 1)
                  pixels_copy[x+1][y] = 1
                  pixels_copy[x-1][y] = 1
                  pixels_copy[x][y-1] = 1
                 end
-                if x == 3 and y != 3
+                if x == (@pixels.length - 1) and y != (@pixels[x].length - 1)
                   pixels_copy[x][y+1] = 1
                   pixels_copy[x][y-1] = 1 
                 end
